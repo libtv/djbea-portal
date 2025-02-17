c203ecc14303e084dadde0db47b332506bfb2d33
libtv <libtv@naver.com>
2025-02-17 20:01:31 +0900

    revise content

--- src/route/home/section/04/home.section-04.tsx
+++ src/route/home/section/04/home.section-04.tsx
@@ -8,45 +8,43 @@ export function HomeSection04() {
         <span>대전시의 다양한 서비스를 분야별로 제공해 드립니다.</span>
       </div>
       <div className="wrap-filed">
-        <div className="field-tab">
-          <ul className="tab-list">
-            <li className="is-current">
-              <a href="javascript:void(0);" data-id="service0">
-                <i>전체</i>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" data-id="service1">
-                <i>일자리 지원</i>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" data-id="service2">
-                <i>중소기업 지원</i>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" data-id="service3">
-                <i>소상공인 지원</i>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" data-id="service4">
-                <i>창업 지원</i>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" data-id="service5">
-                <i>국내·외 마케팅 지원</i>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" data-id="service6">
-                <i>기타</i>
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className="field-tab">
+          <li className="is-current">
+            <a href="javascript:void(0);" data-id="service0">
+              <i>전체</i>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);" data-id="service1">
+              <i>일자리 지원</i>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);" data-id="service2">
+              <i>중소기업 지원</i>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);" data-id="service3">
+              <i>소상공인 지원</i>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);" data-id="service4">
+              <i>창업 지원</i>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);" data-id="service5">
+              <i>국내·외 마케팅 지원</i>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);" data-id="service6">
+              <i>기타</i>
+            </a>
+          </li>
+        </ul>
         <div className="field-body"></div>
       </div>
     </div>
