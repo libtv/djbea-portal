8ed735e1377c7db1a99b6bb22d82be311b5ac12c
libtv <libtv@naver.com>
2025-02-21 17:57:14 +0900

    add menu

--- src/route/menu/menu.context.tsx
+++ src/route/menu/menu.context.tsx
@@ -1,3 +1,13 @@
+import { CommonBoardComponent } from "../../component/common-board/common-board";
+import "../../style/css/menu/menu.context.css";
+
 export function MenuContext() {
-  return <div className="menu-context"></div>;
+  return (
+    <div className="menu-wrap">
+      <div className="menu-context">
+        <h3 className="context-tit">입주공고</h3>
+        <CommonBoardComponent></CommonBoardComponent>
+      </div>
+    </div>
+  );
 }
--- src/route/menu/menu.header.tsx
+++ src/route/menu/menu.header.tsx
@@ -1,3 +1,24 @@
+import "../../style/css/menu/menu.header.css";
+
 export function MenuHeader() {
-  return <div className="menu-context"></div>;
+  return (
+    <div className="menu-header">
+      <h2 className="spot-title">알림홍보</h2>
+      <ul className="con-area">
+        <li className="home">
+          <a href="#">HOME</a>
+        </li>
+        <li className="breadcrumbs-item">
+          <button id="head_menu_depth1_btn" type="button" className="toggle-btn">
+            알림·홍보
+          </button>
+        </li>
+        <li className="breadcrumbs-item">
+          <button id="head_menu_depth1_btn" type="button" className="toggle-btn">
+            기관행사
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
